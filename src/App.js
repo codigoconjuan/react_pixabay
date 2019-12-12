08ddf063945189464f69a3ab1efef464d1f1e24f
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-12 16:40:39 -0600

    primeros pasos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imágenes</p>
+
+        <Formulario />
+      </div>
     </div>
   );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Formulario = () => {
+    return ( 
+        <form>
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="Busca una imagen, ejemplo: futbol o café"
+
+                    />
+                </div>
+                <div className="form-group col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-lg btn-danger btn-block"
+                        value="Buscar"
+                    />
+                </div>
+            </div>
+        </form>
+     );
+}
+ 
+export default Formulario;
