0dc65a78cd64c94276657e08f8d813a1454592e2
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-12 17:32:37 -0600

    calculando total de paginas

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   // state de la app
   const [ busqueda, guardarBusqueda ] = useState('');
   const [ imagenes, guardarImagenes] = useState([]);
+  const [ paginaactual, guardarPaginaActual ] = useState(1);
+  const [ totalpaginas, guardarTotalPaginas] = useState(1);
 
   useEffect(() => {
     const consultarApi = async () => {
@@ -20,6 +22,10 @@ function App() {
         const resultado = await respuesta.json();
 
         guardarImagenes(resultado.hits);
+
+        // calcular el total de paginas
+        const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina );
+        guardarTotalPaginas(calcularTotalPaginas);
     }
     consultarApi();
   }, [busqueda])
